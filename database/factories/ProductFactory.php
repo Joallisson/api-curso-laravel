20df6c19fdcb2ade293cc8a412c803c2f87a33ca
joallisson <joallissonnc@gmail.com>
2022-06-10 10:15:21 -0300

    21. Introdução

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'price' => $this->faker->randomFloat(2, 0, 8),
+            'description' => $this->faker->text,
+            'slug' => $this->faker->slug
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //\App\Models\User::factory(10)->create();
-        $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            ProductTableSeeder::class
+        ]);
+        
     }
 }
